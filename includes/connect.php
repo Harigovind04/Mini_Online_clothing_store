e2722f9af78f7dcb22dfb655206f4593ddf4839b
Harigovind04 <156642476+Harigovind04@users.noreply.github.com>
2024-01-27 22:12:49 +0530

    Add files via upload

---
+++ includes/connect.php
@@ -0,0 +1,17 @@
+<?php
+
+
+  $con=mysqli_connect('localhost','root','','haute_harbor');
+  if($con)
+  {
+      //   echo "connection successful";
+   
+
+   }
+  else{
+     die(mysqli_error($con));
+  }
+
+   
+
+?>
